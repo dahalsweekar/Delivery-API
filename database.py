1082cbc7a47e4f66fa3012f5718ad30923a546c3
Sweekar <dahalsweekar@gmail.com>
2024-05-13 10:06:40 +0545

    Create database session

---
+++ database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQL_URL = "sqlite:///./my_sql.db"
+engine = create_engine(SQL_URL, echo=True)
+
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
